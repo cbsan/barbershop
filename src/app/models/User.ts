6f259bb79695f225f8b01eb3a8bac25b41622f1a
cbsan <cbsan.dev@gmail.com>
2019-06-04 13:15:07 -0300

    user

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,4 +1,4 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize, DataTypes } from 'sequelize'
 
 export interface UserInterface {
   id?: number
@@ -9,16 +9,13 @@ export interface UserInterface {
   provider: boolean
 }
 
-type UserInstance = Sequelize.Instance<UserInterface> & UserInterface
-type User = Sequelize.Model<UserInstance, UserInterface>
-
-export default (sequelize, DataTypes) => {
+export default (sequelize: Sequelize, dataTypes: DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    avatar: DataTypes.STRING,
-    passwordHash: DataTypes.STRING,
-    provider: DataTypes.BOOLEAN
+    name: dataTypes.STRING,
+    email: dataTypes.STRING,
+    avatar: dataTypes.STRING,
+    passwordHash: dataTypes.STRING,
+    provider: dataTypes.BOOLEAN
   })
 
   return User
