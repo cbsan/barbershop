fc93239b506ca48455593effc3f1a74a16ba7f64
cbsan <cbsan.dev@gmail.com>
2019-06-05 13:30:51 -0300

    create user

--- src/routes/controllers/UserController.ts
+++ src/routes/controllers/UserController.ts
@@ -1,6 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const User = require('../../app/models/User')
+
 class UserController {
   create (req, res) {
     return res.render('auth/signup')
+  }
+
+  async store (req, res) {
+    const { body } = req
+
+    await User.create(body)
+
+    return res.redirect('/')
   }
 }
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,5 +5,6 @@ import UserController from './controllers/UserController'
 const routes = express.Router()
 
 routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 export default routes
